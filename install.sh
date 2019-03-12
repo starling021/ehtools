3ae17cba6d149caf70728c5884dc03ee43b342fa
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-03-13 00:17:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -329,6 +329,38 @@ read -p $'(\e[4;93m(new)password\e[0m\en)> ' passwords
 touch password
 echo $passwords >> /etc/ehtools/password
 
+echo -e
+
+function ProgressBar {
+
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+                         
+        
+printf "\rConfiguring: [${_fill// /\#}${_empty// /-}] ${_progress}%%"
+
+}
+
+
+_start=1
+
+_end=100
+
+for number in $(seq ${_start} ${_end})
+do
+    sleep 0.2
+    ProgressBar ${number} ${_end}
+done
+printf '\nFinished!\n'
+sleep 5
+clear
+fi
+
 sleep 5
 
 clear
