16875383217af0153fc034b33b39f952408f1d29
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-03-13 02:46:53 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -34,6 +34,70 @@
 #
 
 
+#yellow start 
+	YS="\e[1;33m"
+#blue start 
+	BS="\e[0;34m"
+#color end
+	CE="\e[0m"
+#red start
+	RS="\e[1;31m"
+#black start
+	BLS="\e[0;30m"
+#dark gray start
+	DGYS="\e[1;30m"
+#light blue start
+	LBS="\e[1;34m"
+#green start
+	GNS="\e[0;32m"
+#light green start
+	LGNS="\e[1;32m"
+#cyan start
+	CYS="\e[0;36m"
+#light cyan start
+	LCYS="\e[1;36m"
+#light red start
+	DRS="\e[0;31m"
+#purple start
+	PS="\e[0;35m"
+#light purple start
+	LPS="\e[1;35m"
+#brown start
+	BRS="\e[0;33m"
+#light gray start
+	LGYS="\e[0;37m"
+#white start
+	WHS="\e[1;37m"
+
+
+echo -e "$YS"Ehtools Authentication"$CE"
+
+sleep 0.5
+read -p $'(\e[4;93mlogin\e[0m\en)> ' loginer
+logins=$(cat /etc/ehtools/login)
+while [ "$loginer" != "$logins" ]
+do
+echo -e "$RS"Wrong login!"$CE"
+sleep 3
+echo -e "("$YS"Try again..."$CE")"
+sleep 2
+read -p $'(\e[4;93mlogin\e[0m\en)> ' loginer
+
+done
+
+sleep 0.5
+read -p $'(\e[4;93mpassword\e[0m\en)> ' passworder
+passwords=$(cat /etc/ehtools/password)
+while [ "$passworder" != "$passwords" ]
+do
+echo -e "$RS"Wrong password!"$CE"
+sleep 3
+echo -e "("$YS"Try again..."$CE")"
+sleep 2
+read -p $'(\e[4;93mpassword\e[0m\en)> ' passworder
+
+done
+
 function nounistall
 {
 	echo -e "Oops, why you want to uninstall me? "
