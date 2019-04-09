145d8a102178bde76102085bb8466819a63c490b
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-04-09 21:36:32 +0300

    Update l133.sh

--- ls/l133.sh
+++ ls/l133.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
+
+{
+KEYEK="$( cat /etc/ehtools/root/service/ehtkey.txt )"
+KEYKE="$( curl "https://raw.githubusercontent.com/entynetproject/ECLI/master/hello.txt" 2>/dev/null )"
+} &> /dev/null
+
+if [[ "$KEYEK" != "$KEYKE" ]]
+then
+
+sleep 1
+echo -e "["$RS"*"$CE"] "$RS"Failed to open session for l133.sh, did you buy ehtools?"$CE""
+sleep 1
+exit
+
+fi
+
 arpspoof -i $GATEINT -t $GATENM $TARGIP
 read
