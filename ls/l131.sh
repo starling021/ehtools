55eefca6fef2a8c31c194340dce83a47f424a25e
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-04-09 21:35:19 +0300

    Update l131.sh

--- ls/l131.sh
+++ ls/l131.sh
@@ -1,4 +1,20 @@
 #! /bin/bash
+
+{
+KEYEK="$( cat /etc/ehtools/root/service/ehtkey.txt )"
+KEYKE="$( curl "https://raw.githubusercontent.com/entynetproject/ECLI/master/hello.txt" 2>/dev/null )"
+} &> /dev/null
+
+if [[ "$KEYEK" != "$KEYKE" ]]
+then
+
+sleep 1
+echo -e "["$RS"*"$CE"] "$RS"Failed to open session for l131.sh, did you buy ehtools?"$CE""
+sleep 1
+exit
+
+fi
+
 cd
 cd dns2proxy
 python /root/dns2proxy/dns2proxy.py
