0914bdaea4f2f22524372dab3e37166386019558
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-04-26 18:25:04 +0300

    Update l131.sh

--- ls/l131.sh
+++ ls/l131.sh
@@ -1,4 +1,32 @@
 #! /bin/bash
+
+# 
+#            --------------------------------------------------
+#                            Ehtools Framework          
+#            --------------------------------------------------
+#                  Copyright (C) <2015>  <Ehtools Team>
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    
+#    
+#    About Author :
+#    Founder   : Ehtools Team
+#    Site      : https://ehtools.pro/
+#    Instagram : @ehtools_framework
+#    Twitter   : @ehtools
+#
+
 cd
 cd dns2proxy
 python /root/dns2proxy/dns2proxy.py
