02013679d5eae1a74dd869a462aa54fd3b874082
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-22 13:23:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -288,6 +288,7 @@ fi
 done
 clear
 printf '\033]2;ehtools INSTALLER\a'
+echo DO_NOT_DELETE_THIS_CONFIG_FILE >> /etc/ehtools/.configuring
 echo -e "Are you want to install modules now?(\e[1;33myes\e[0m/\e[1;33mno\e[0m):"
 echo -e "If you do not install the modules now, then most of the functions will not work!"
 echo -e "So without thinking install them now!"
@@ -464,7 +465,6 @@ echo -e "=> "$YS"you must not say this key someone such as "$CE"["$YS"your frien
 echo -e "This indenty key will protect ehtools boot!"
 read -p $'(\e[4;93mkey\e[0m\en)> ' DADI
 echo $DADI >> /root/ehtools/etc/config
-echo DO_NOT_DELETE_THIS_CONFIG_FILE >> /etc/ehtoolslite/.configuring
 
 clear
 printf '\033]2;ehtools INSTALLER\a'
