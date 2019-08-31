095645bc91ead45ee6dcdb0c891635ad0b895596
Ivan Nikolsky <54115104+entydev@users.noreply.github.com>
2019-08-31 11:07:33 +0300

    Add files via upload

---
+++ etc/lite/ls/l131.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# 
+#            --------------------------------------------------
+#                            Ehtools Framework          
+#            --------------------------------------------------
+#                  Copyright (C) <2015>  <Ehtools Team>
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    
+#    
+#    About Author :
+#    Founder   : Ehtools Team
+#    Site      : https://ehtools.pro/
+#    Instagram : @ehtools_framework
+#    Twitter   : @ehtools
+#
+
+CE="\e[0m"
+RS="\e[1;31m"
+YS="\e[1;33m"
+
+WHO="$( whoami )"
+
+if [[ "$WHO" != "root" ]]
+then
+sleep 1
+echo -e "$RS"run it as"$CE" "$YS"root"$CE"
+sleep 1
+echo -e "$RS"or use"$CE" "$YS"sudo"$CE"
+sleep 1
+exit
+fi
+
+cd
+cd dns2proxy
+python /root/dns2proxy/dns2proxy.py
+read
