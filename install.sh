c0b434cb4f057abb7954578e58c8ea5e1537433e
Ivan Nikolsky <54115104+entydev@users.noreply.github.com>
2019-08-31 11:38:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,52 +79,6 @@ cd /root/ehtools
 fi
 fi
 
-if [[ -d /root/ehtoolslite ]]
-then
-{
-cd /root/ehtoolslite
-chmod +x uninstall.sh
-cd lib
-chmod +x libunlogin
-./libunlogin
-chmod +x libunchattr
-./libunchattr
-chmod +x libunconf
-./libunconf
-rm /etc/ehtoolslite/login
-rm /etc/ehtoolslite/password
-touch /etc/ehtoolslite/login
-touch /etc/ehtoolslite/password
-cd /root/ehtoolslite
-
-              cd /root/ehtoolslite/lib
-	      chmod +x libunchattr
-	      ./libunchattr
-	      cd /root/ehtoolslite/lib
-              chmod +x libunconf
-              ./libunconf
-	      rm -r /root/ehtoolslite
-	      rm -r /bin/ehtoolslite
-	      rm -r /etc/ehtoolslite
-	      rm -r /root/handshakes
-	      rm -r /root/wordlists
-	      rm /bin/eht1
-	      rm /bin/eht2
-	      rm /bin/eht3
-	      rm /bin/eht4
-	      rm /bin/eht21
-	      rm /bin/eht31
-	      rm /bin/eht41
-	      rm /bin/eht42
-	      rm /bin/eht43
-	      rm /bin/epasswd
-	      rm /bin/modules
-	      rm /bin/euh
-	      rm /bin/uiecache
-	      
-} &> /dev/null
-fi
-
 {
 chattr -i /tmp/config
 chattr -i /tmp/config/config.txt
