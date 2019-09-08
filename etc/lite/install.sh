06d9383296932c3894680df1b68aa4e68add605c
Ivan Nikolsky <54115104+entydev@users.noreply.github.com>
2019-09-08 12:24:41 +0300

    Update install.sh

--- etc/lite/install.sh
+++ etc/lite/install.sh
@@ -293,7 +293,7 @@ clear
 printf '\033]2;ehtools password changer\a'
 echo -e "Set ehtools password"
 echo -e "Let's create ehtools password!"
-echo -e "Ehtools protection:"
+echo -e "Ehtools Password Protection:"
 
 sleep 0.5
 read -p $'(\e[4;93m(new)login\e[0m\en)> ' logins
