1f06fac19ec2e702ad406afe1bb3c65958fc1d1f
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-09-17 22:22:03 +0300

    Update install.sh

--- etc/pro/install.sh
+++ etc/pro/install.sh
@@ -132,7 +132,7 @@ fi
 
 else
 sleep 1
-echo -e "["$RS"*"$CE"] "$RS"Can't connect with server: There is no connection!"$CE""
+echo -e "["$RS"*"$CE"] "$RS"Can't connect to server: There is no connection!"$CE""
 sleep 1
 exit
 fi
